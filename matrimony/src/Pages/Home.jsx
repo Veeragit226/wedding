94a2b93607281970b6787b5778c5de18d0ed7e1a
Veeragit226 <joker991410@gmail.com>
2024-10-29 22:08:27 +0530

    Added navbaar

---
+++ matrimony/src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+function Home() {
+  return (
+    <>
+       <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button class="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">...</button>
+    </>
+  )
+}
+
+export default Home
