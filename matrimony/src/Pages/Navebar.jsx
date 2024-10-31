c2211a6ef7022c90f606ccd51724ba57d2316226
Veeragit226 <joker991410@gmail.com>
2024-10-31 16:53:17 +0530

    home nav section some changes

--- matrimony/src/Pages/Navebar.jsx
+++ matrimony/src/Pages/Navebar.jsx
@@ -12,17 +12,19 @@ const navigation = [
   { name: 'PLANES', href: '#' },
   { name: 'REGISTER', href: '#' },
   { name: 'DASHBOARD', href: '#' }
+
 ]
 
 export default function Navebar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
-  const [hovering, setHovering]=useState(false)
+  const [hovering, setHovering] = useState(false)
 
   return (
     <>
-      <header className="absolute inset-x-0 top-0 z-50  shadow-md ">
-        <nav aria-label="Global" className="mx-auto max-w-7xl px-5 flex items-center justify-between p-6 lg:px-8">
+      <header className="sticky bg-white inset-x-0 top-0 z-50  shadow-md ">
+        <nav aria-label="Global" className="mx-auto max-w-7xl px-5 flex items-center justify-between p-4 lg:px-8">
           <div className="flex lg:flex-1">
+
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
               <img
@@ -32,31 +34,33 @@ export default function Navebar() {
               />
             </a>
           </div>
-          <div className="flex lg:hidden ">
+          <div className="flex lg:hidden  ">
             <button
               type="button"
               onClick={() => setMobileMenuOpen(true)}
-              className="-m-2.5 inline-flex items-center justify-center rounded-full p-2.5 text-white bg-yellow-600"
+              className="-m-2.5 inline-flex items-center justify-center rounded-full p-2.5 text-white  animate-ping relative  bg-yellow-600"
             >
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon aria-hidden="true" className="h-6 w-6" />
+              <span className=" sr-only">Open main menu</span>
+              <Bars3Icon aria-hidden="true" className=" h-6 w-6" />
+            </button>
+
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(true)}
+              className="-m-2.5 inline-flex absolute items-center justify-center rounded-full p-2.5 text-white  bg-yellow-600"
+            >
+              <span className=" sr-only">Open main menu</span>
+              <Bars3Icon aria-hidden="true" className=" h-6 w-6" />
             </button>
           </div>
-          <div className="hidden lg:flex lg:gap-x-8">
+          <div className="hidden ps-20 lg:flex lg:gap-x-8">
             {navigation.map((item) => (
-              <a key={item.name} href={item.href} className="text-sm/1  text-yellow-900">
+              <a key={item.name} href={item.href} className="text-sm/5  text-yellow-900">
                 {item.name}
               </a>
             ))}
           </div>
-          
-
-
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            {/* <a href="#" className="text-sm/6 font-semibold text-gray-900">
-              Log in <span aria-hidden="true">&rarr;</span>
-            </a> */}
-
             <Menu as="div" className="relative ml-3">
               <div>
                 <MenuButton className="relative  flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
@@ -90,18 +94,11 @@ export default function Navebar() {
                 </MenuItem>
               </MenuItems>
             </Menu>
-
           </div>
-
-       
-
-
         </nav>
         <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
-
-          
           <div className="fixed inset-0 z-50" />
-          <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <DialogPanel className="fixed  inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">Your Company</span>
@@ -127,25 +124,18 @@ export default function Navebar() {
                     <a
                       key={item.name}
                       href={item.href}
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base/5 text-yellow-900 hover:bg-gray-50"
                     >
                       {item.name}
                     </a>
                   ))}
                 </div>
                 <div className="py-6">
-                  {/* <a
-                    href="#"
-                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                  >
-                    Log in
-                  </a> */}       
                 </div>
               </div>
             </div>
           </DialogPanel>
         </Dialog>
-
       </header>
     </>
   )
